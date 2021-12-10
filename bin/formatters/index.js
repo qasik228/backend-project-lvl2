1b47fc89f3f0770009a8ede33393d67c74c1e061
Rostislav <qasik228@yandex.ru>
2021-12-10 15:05:33 +0700

    step

--- bin/formatters/index.js
+++ bin/formatters/index.js
@@ -2,16 +2,24 @@ import stylish from './stylish.js';
 import plain from './plain.js';
 
 const format = (compFile, formatName) => {
-  switch (formatName) {
-    case 'stylish':
-      return stylish(compFile);
-    case 'plain':
-      return plain(compFile);
-    case 'json':
-      return JSON.stringify(compFile);
-    default:
-      throw new Error(`This format is not supported: ${formatName}`);
+  if (formatName === 'stylish') {
+    return stylish(compFile);
+  } if (formatName === 'plain') {
+    return plain(compFile);
+  } if (formatName === 'json') {
+    return JSON.stringify(compFile);
   }
+  throw new Error(`This format is not supported: ${formatName}`);
+  // switch (formatName) {
+  //   case 'stylish':
+  //     return stylish(compFile);
+  //   case 'plain':
+  //     return plain(compFile);
+  //   case 'json':
+  //     return JSON.stringify(compFile);
+  //   default:
+  //     throw new Error(`This format is not supported: ${formatName}`);
+  // }
 };
 
 export default format;
